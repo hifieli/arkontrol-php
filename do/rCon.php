876b12fb4627b5f93d08e9a18cc5ceea21859f85
hifieli2@gmail.com <hifieli2@gmail.com>
2015-08-13 09:31:27 -0700

    Committed at Thu Aug 13 09:31:27 MST 2015

--- do/rCon.php
+++ do/rCon.php
@@ -3,13 +3,16 @@
 	include_once('../init.php');
 	
 	//do things.
-	
-	include_once($_INICONF['webdocroot'] . '/includes/gameuserini.php');	//$gameuserini
-	
 	require_once($_INICONF['webdocroot'] . '/includes/valve_rcon.php');
 	
+	
+	
+	//include_once($_INICONF['webdocroot'] . '/includes/gameuserini.php');	//$gameuserini
+	//$rCon = new Valve_RCON($gameuserini['GameUserSettings.ini']['ServerSettings']['ServerAdminPassword'], '127.0.0.1', 32330, Valve_RCON::PROTO_CLASSIC);
+	$gameuserini = @parse_ini_file($_INICONF['settingspath'] . "/GameUserSettings.ini", true);
 	$rCon = new Valve_RCON($gameuserini['ServerSettings']['ServerAdminPassword'], '127.0.0.1', 32330, Valve_RCON::PROTO_CLASSIC);
 
+	
 	$rCon->connect();
 	$rCon->authenticate();
 	$response = $rCon->execute('status');
