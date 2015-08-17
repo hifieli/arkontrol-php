a94ae3ba9f62f60876813c38fb4778f0e9f3d560
hifieli2@gmail.com <hifieli2@gmail.com>
2015-08-16 21:30:41 -0700

    Committed at Sun Aug 16 21:30:40 MST 2015

--- do/rCon.php
+++ do/rCon.php
@@ -12,16 +12,16 @@
 	$go_ahead	= false;
 	try {
 		
-		if (empty($gameuserini['ServerSettings']['ServerAdminPassword']))	throw new \Exception("'ServerAdminPassword' is required.");
-		if (empty($gameuserini['ServerSettings']['RCONEnabled']))			throw new \Exception("'RCONEnabled' must be set to 'true'.");
-		if (empty($gameuserini['ServerSettings']['RCONPort']))				throw new \Exception("'RCONPort' not set.");
+		if (empty($gameuserini['ServerSettings']['ServerAdminPassword']))	throw new \Exception("'ServerAdminPassword' is required." . ' You can adjust your RCON settings in the <a href="/do/serverConfig.php#authentication">Server Configuration Interface</a>.');
+		if (empty($gameuserini['ServerSettings']['RCONEnabled']))			throw new \Exception("'RCONEnabled' must be set to 'true'." . ' You can adjust your RCON settings in the <a href="/do/serverConfig.php#authentication">Server Configuration Interface</a>.');
+		if (empty($gameuserini['ServerSettings']['RCONPort']))				throw new \Exception("'RCONPort' not set.". ' You can adjust your RCON settings in the <a href="/do/serverConfig.php#authentication">Server Configuration Interface</a>.');
 		
 	//	if (!empty($_REQUEST) && (empty($_REQUEST['rcon-cmd-string'])))		throw new \Exception("No command issued."); //check for a command in $_POST
 		
 		if (!empty($_SESSION['need_to_restart']))							throw new \Exception("Server is pending a restart to apply updated configuration.");
 		
 		$server_status_raw	= exec("service {$_INICONF['servicename']} status");
-		if ( strstr($server_status_raw, 'running') === false )				throw new \Exception("Server is not running.");
+		if ( strstr($server_status_raw, 'running') === false )				throw new \Exception("Server is not running." . ' You can start the server in the <a href="/do/serverDetails.php">Server Control Interface</a>.');
 				
 		require_once($_INICONF['webdocroot'] . '/includes/data/rconhelp.php');
 		
@@ -33,7 +33,7 @@
 	} catch (\Exception $e) {
 		
 		$go_ahead	= false;
-		$_MSGS[]	= array('type'=>'error', 'msg'=> 'rCon is currently unavailable: ' . $e->getMessage() . ' You can adjust your RCON settings in the <a href="/do/serverConfig.php#authentication">Server Configuration Interface</a>.');
+		$_MSGS[]	= array('type'=>'error', 'msg'=> 'rCon is currently unavailable: ' . $e->getMessage());
 	
 	}
 	
